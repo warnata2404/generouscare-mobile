f528d4f8fe03d7c5ad6a33e4ef562bbf864b88c0
warnata2404 <411231103@mahasiswa.undira.ac.id>
2026-06-14 16:16:37 +0700

    auth: integrate auth provider into root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
+import { AuthProvider } from "@/contexts/AuthContext";
+
 export default function RootLayout() {
   return (
-    <>
+    <AuthProvider>
       <Stack
         screenOptions={{
           headerShown: false,
@@ -11,6 +13,6 @@ export default function RootLayout() {
       />
 
       <StatusBar style="auto" />
-    </>
+    </AuthProvider>
   );
 }
